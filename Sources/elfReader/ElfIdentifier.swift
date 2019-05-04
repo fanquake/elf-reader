6003b893e5b52fa1b6cf53cb8e65a5a7ceb59178
fanquake <fanquake@gmail.com>
2019-05-04 19:44:20 +0800

    Add missing e_type identifiers

--- Sources/elfReader/ElfIdentifier.swift
+++ Sources/elfReader/ElfIdentifier.swift
@@ -19,6 +19,8 @@ extension elfReader {
         case ET_EXEC = 0x02
         case ET_DYN = 0x03
         case ET_CORE = 0x04
+        case ET_LOOS = 0xfe00 // 65024
+        case ET_HIOS = 0xfeff // 65279
         case ET_LOCPROC = 0xff00 // 65280
         case ET_HIPROC = 0xffff // 65535
 
@@ -34,9 +36,9 @@ extension elfReader {
                 return "Shared object file"
             case .ET_CORE:
                 return "Core file"
-            case .ET_LOCPROC:
-                return "Processor-specific"
-            case .ET_HIPROC:
+            case .ET_LOOS, .ET_HIOS:
+                return "Operating system-specific"
+            case .ET_LOCPROC, .ET_HIPROC:
                 return "Processor-specific"
             }
         }
